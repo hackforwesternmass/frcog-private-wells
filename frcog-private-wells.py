a6448c26486ec9f41a74c279bc5e846b4c5704ad
dgarant <dangarant@gmail.com>
2013-06-01 15:44:37 -0400

    Laying out basic request structure

--- frcog-private-wells.py
+++ frcog-private-wells.py
@@ -1,10 +1,34 @@
-
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/wells')
+def wells():
+    return render_template('well-list.html')
+
+@app.route('/wells/edit')
+def edit_well():
+    return render_template('edit-well.html')
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@app.route('/reports/edit')
+def edit_report():
+    return render_template('edit-report.html')
+
+@app.route('/quality-measures')
+def edit_quality_measure_list():
+    return render_template('edit-quality-measures.html')
+    
 
