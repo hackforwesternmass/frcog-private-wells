5447c611f7d329fdc3ce540b8273d41417a0959f
Russell Pitre <rpitre@gmail.com>
2013-06-02 04:00:43 -0400

    more csv parsing work

--- database/wells/run.js
+++ database/wells/run.js
@@ -9,28 +9,31 @@ var fs = require('fs');
 // http://www.adaltas.com/projects/node-csv/
 var csv = require('csv');
 
-// https://github.com/brianc/node-postgres
-var pg = require('pg').native;
+// delete out file
+var outfile = 'wells_insert.sql';
 
+fs.unlink(outfile, function (err) {
+    //if (err) throw err;
+    console.log('successfully deleted ' + outfile);
+});
 
-var conString = "tcp://crwpuwcgrodlii:tXiOYzbzVQJOlxVhyndPgt9Aof@ec2-23-21-130-168.compute-1.amazonaws.com/dcrmabu3rp1e14";
-
-var client = new pg.Client(conString);
-//hclient.connect();
-
-String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+var towns = ['buckland', 'charlemont', 'gill'];
 
 function isBlank(str) {
     return (!str || /^\s*$/.test(str));
 }
 
-function varchar(str) {
+function varchar(str, len) {
     var value = '';
 
     if (isBlank(str)) {
-        value = '';
+        return 'null';
     } else {
         value =  str.replace(/'/g, "").replace(/"/g, "");
+    }
+
+    if(len) {
+        value = value.substring(0, len);
     }
 
     return  "'" + value + "'";
@@ -48,7 +51,13 @@ function integer(str) {
     return (str != null && str !== '') ? str : 'null';
 }
 
-var sql_insert = "INSERT INTO wells(" +
+function mapMuni(str) {
+    if (str.toUpperCase() === 'GILL') return 109;
+    else if (str.toUpperCase() === 'BUCKLAND') return 48;
+    else if (str.toUpperCase() === 'CHARLEMONT') return 54;
+}
+
+var sql_insert = "INSERT INTO wells_well (" +
     "dep_well_id, " +
     "owner_name, " +
     "owner_street_1, " +
@@ -74,115 +83,65 @@ var sql_insert = "INSERT INTO wells(" +
     "municipalities_id," +
     "well_types_id," +
     "created_by," +
-        "modified_by) VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);";
+        "modified_by) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);";
 
-/**
- * charlemont.csv
- * buckland.csv
- * gill.csv
- */
 
-csv()
-.from.path(__dirname+'/csv/gill.csv', {
-  columns: true
+var sqlFile = fs.createWriteStream(outfile, {
+    flags: "a",
+    encoding: "encoding",
+    mode: 0744
 })
-.transform( function(row){
 
-        var statement = util.format(sql_insert,
-            integer(row.WELL_COMPLETION_ID),
-            varchar(row.PROPERTY_OWNER),
-            varchar(row.OWNER_STREET_NUMBER),
-            varchar(row.OWNER_STREET_NAME),
-            varchar(row.OWNER_CITY),
-            varchar(''),
-            varchar(''),
-            varchar(row.WELL_STREET_NUMBER),
-            varchar(row.WELL_STREET_NAME),
-            float(row.LATITUDE),
-            float(row.LONGITUDE),
-            varchar(row.WC_DEPTH),
-            integer(row.DEPTH_TO_BEDROCK),
-            varchar(row.ASSESSORS_MAP),
-            varchar(row.ASSESSORS_LOT),
-            varchar(row.BOH_DATE_ISSUED),
-            varchar(row.BOH_PERMIT),
-            varchar(row.COMMENTS),
-            date(row.WC_DATE),
-            varchar(row.FIRM),
-            varchar(row.SUPERVISING_DRILLER),
-            varchar(''),
-            109,
-            2,
-            1,
-            1);
+for (var i = 0 ; i < towns.length ; i++) {
+    extract(towns[i]);
+}
 
-        /*
-        * //{ WELL_COMPLETION_ID: '282860',
-         // WELL_LOCATION: '',
-         // WELL_STREET_NUMBER: '',
-         // WELL_STREET_NAME: 'West Gill Road',
-         // TOWN_NAME: 'Gill',
-         // PROPERTY_OWNER: 'Robert Whitney',
-         // WELL_SUB_NAME: '',
-         // OWNER_ADDRESS: '',
-         // OWNER_STREET_NUMBER: '',
-         // OWNER_STREET_NAME: '',
-         // OWNER_CITY: 'Gill',
-         // ASSESSORS_LOT: '',
-         // ASSESSORS_MAP: '',
-         // BOH_DATE_ISSUED: '',
-         // DEPTH_TO_BEDROCK: '12',
-         // BOH_PERMIT: 'NR',
-         // WC_DATE: '22-Jul-74',
-         // COMMENTS: 'Steel Casing Type actually 19.45 pounds.',
-         // FIRM: 'Stavens Brothers, Inc.',
-         // WELLTYPE: 'DMST',
-         // SUPERVISING_DRILLER: 'Robert C. Stavens',
-         // WC_DEPTH: '210',
-         // LATITUDE: '',
-         // LONGITUDE: '',
-         LATD: '',
-         LATM: '',
-         LONGD: '',
-         LONGM: '',
-         WELL_LOT_NUM: '' }
+function extract(town) {
 
-         * */
-  console.log(statement);
+    csv()
+    .from.path(__dirname+'/csv/'+town+'.csv', {
+      columns: true
+    })
+    .transform( function(row){
 
-})
-.on('record', function(row, index){
-  console.log(row);
-})
-.on('end', function(count){
-  //console.log('Number of lines: '+count);
-})
-.on('error', function(error){
-  console.log(error.message);
-});
+            var statement = util.format(sql_insert,
+                integer(row.WELL_COMPLETION_ID),
+                varchar(row.PROPERTY_OWNER, 100),
+                varchar(row.OWNER_STREET_NUMBER, 100),
+                varchar(row.OWNER_STREET_NAME, 100),
+                varchar(row.OWNER_CITY, 100),
+                varchar(''),
+                varchar(''),
+                varchar(row.WELL_STREET_NUMBER, 100),
+                varchar(row.WELL_STREET_NAME, 100),
+                float(row.LATITUDE),
+                float(row.LONGITUDE),
+                integer(row.WC_DEPTH),
+                integer(row.DEPTH_TO_BEDROCK),
+                varchar(row.ASSESSORS_MAP, 10),
+                varchar(row.ASSESSORS_LOT, 10),
+                date(row.BOH_DATE_ISSUED),
+                varchar(row.BOH_PERMIT, 100),
+                varchar(row.COMMENTS, 100),
+                date(row.WC_DATE),
+                varchar(row.FIRM, 100),
+                varchar(row.SUPERVISING_DRILLER, 100),
+                varchar(''),
+                mapMuni(town),
+                2,
+                1,
+                1);
 
+    sqlFile.write(statement + '\n');
+    })
+    .on('record', function(row, index){
+      console.log(row);
+    })
+    .on('end', function(count){
+      //console.log('Number of lines: '+count);
+    })
+    .on('error', function(error){
+      console.log(error.message);
+    });
 
-
-
-
-
-
-
-/**
- *
- */
-
-/*
-var query = client.query("SELECT * FROM wells_types");
-
-//can stream row results back 1 at a time
-query.on('row', function(row) {
-  console.log(row);
-});
-
-//fired after last row is emitted
-query.on('end', function() { 
-  client.end();
-});
-
-*/
+}
