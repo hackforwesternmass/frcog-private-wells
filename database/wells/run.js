33d6fd4e5852a2e22bb372ae22dc849fd98c415e
Russell Pitre <rpitre@gmail.com>
2013-06-02 02:50:45 -0400

    csv parsing

--- database/wells/run.js
+++ database/wells/run.js
@@ -18,8 +18,63 @@ var conString = "tcp://crwpuwcgrodlii:tXiOYzbzVQJOlxVhyndPgt9Aof@ec2-23-21-130-1
 var client = new pg.Client(conString);
 //hclient.connect();
 
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
 
-var sql_insert = "INSERT INTO wells(dep_well_id, owner_name, owner_street_1, owner_street_2,owner_city, owner_state, owner_zip, well_street_1, well_street_2,latitude, longitude, total_depth, depth_to_bedrock, assessors_map,assessors_lot, boh_date_issued, boh_permit, comments, wc_date,firm, supervising_driller, field_notes, municipalities_id, well_types_id,created_on, created_by, modified_on, modified_by) VALUES (%s, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?, ?,?, ?, ?, ?);";
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
+
+function varchar(str) {
+    var value = '';
+
+    if (isBlank(str)) {
+        value = '';
+    } else {
+        value =  str.replace(/'/g, "").replace(/"/g, "");
+    }
+
+    return  "'" + value + "'";
+}
+
+function date(str) {
+    return 'null';
+}
+
+function float(str) {
+    return (str != null && str !== '') ? str : 'null';
+}
+
+function integer(str) {
+    return (str != null && str !== '') ? str : 'null';
+}
+
+var sql_insert = "INSERT INTO wells(" +
+    "dep_well_id, " +
+    "owner_name, " +
+    "owner_street_1, " +
+    "owner_street_2," +
+    "owner_city, " +
+    "owner_state, " +
+    "owner_zip, " +
+    "well_street_1," +
+    "well_street_2," +
+    "latitude," +
+    "longitude," +
+    "total_depth," +
+    "depth_to_bedrock," +
+    "assessors_map," +
+    "assessors_lot," +
+    "boh_date_issued," +
+    "boh_permit," +
+    "comments, " +
+    "wc_date," +
+    "firm," +
+    "supervising_driller," +
+    "field_notes," +
+    "municipalities_id," +
+    "well_types_id," +
+    "created_by," +
+        "modified_by) VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);";
 
 /**
  * charlemont.csv
@@ -28,38 +83,79 @@ var sql_insert = "INSERT INTO wells(dep_well_id, owner_name, owner_street_1, own
  */
 
 csv()
-.from.path(__dirname+'/csv/ashfield.csv', {
+.from.path(__dirname+'/csv/gill.csv', {
   columns: true
 })
 .transform( function(row){
 
+        var statement = util.format(sql_insert,
+            integer(row.WELL_COMPLETION_ID),
+            varchar(row.PROPERTY_OWNER),
+            varchar(row.OWNER_STREET_NUMBER),
+            varchar(row.OWNER_STREET_NAME),
+            varchar(row.OWNER_CITY),
+            varchar(''),
+            varchar(''),
+            varchar(row.WELL_STREET_NUMBER),
+            varchar(row.WELL_STREET_NAME),
+            float(row.LATITUDE),
+            float(row.LONGITUDE),
+            varchar(row.WC_DEPTH),
+            integer(row.DEPTH_TO_BEDROCK),
+            varchar(row.ASSESSORS_MAP),
+            varchar(row.ASSESSORS_LOT),
+            varchar(row.BOH_DATE_ISSUED),
+            varchar(row.BOH_PERMIT),
+            varchar(row.COMMENTS),
+            date(row.WC_DATE),
+            varchar(row.FIRM),
+            varchar(row.SUPERVISING_DRILLER),
+            varchar(''),
+            109,
+            2,
+            1,
+            1);
 
-        console.log(util.format(sql_insert, row.WELL_ID));
+        /*
+        * //{ WELL_COMPLETION_ID: '282860',
+         // WELL_LOCATION: '',
+         // WELL_STREET_NUMBER: '',
+         // WELL_STREET_NAME: 'West Gill Road',
+         // TOWN_NAME: 'Gill',
+         // PROPERTY_OWNER: 'Robert Whitney',
+         // WELL_SUB_NAME: '',
+         // OWNER_ADDRESS: '',
+         // OWNER_STREET_NUMBER: '',
+         // OWNER_STREET_NAME: '',
+         // OWNER_CITY: 'Gill',
+         // ASSESSORS_LOT: '',
+         // ASSESSORS_MAP: '',
+         // BOH_DATE_ISSUED: '',
+         // DEPTH_TO_BEDROCK: '12',
+         // BOH_PERMIT: 'NR',
+         // WC_DATE: '22-Jul-74',
+         // COMMENTS: 'Steel Casing Type actually 19.45 pounds.',
+         // FIRM: 'Stavens Brothers, Inc.',
+         // WELLTYPE: 'DMST',
+         // SUPERVISING_DRILLER: 'Robert C. Stavens',
+         // WC_DEPTH: '210',
+         // LATITUDE: '',
+         // LONGITUDE: '',
+         LATD: '',
+         LATM: '',
+         LONGD: '',
+         LONGM: '',
+         WELL_LOT_NUM: '' }
 
-/*        { WELL_ID: '\n',
-            TOWN: null,
-            STREET_NUMBER: null,
-            STREET_NAME: null,
-            LATITUDE: null,
-            LONGITUDE: null,
-            DATE_COMPLETE: null,
-            WELL_TYPE: null,
-            WORKPERFORMED: null,
-            TOTAL_DEPTH: null,
-            DEPTH_TO_BEDROCK: null,
-            WATER_LEVEL: null
-        }*/
+         * */
+  console.log(statement);
 
-   console.log("")
-
-    console.log(row);
-  return row;
 })
-.on('record', function(row,index){
-  console.log('#'+index+' '+JSON.stringify(row));
+.on('record', function(row, index){
+  console.log(row);
 })
 .on('end', function(count){
-  console.log('Number of lines: '+count);
+  //console.log('Number of lines: '+count);
 })
 .on('error', function(error){
   console.log(error.message);
