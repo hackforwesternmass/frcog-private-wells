2c6fe2941195ce57dd44e51c48a754c432f50bc2
Jason Leveille <jason@leve.us>
2013-06-02 11:47:35 -0400

    Updated save model for municipality admin.

--- frcog/municipalities/admin.py
+++ frcog/municipalities/admin.py
@@ -5,5 +5,12 @@ from .models import Municipality
 class MunicipalityAdmin(admin.ModelAdmin):
     list_display = ('__unicode__',)
     list_filter = ['created_on', 'modified_on']
+    exclude = ['created_by', 'modified_by']
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.modified_by = request.user
+            obj.save()
 admin.site.register(Municipality, MunicipalityAdmin)
 
