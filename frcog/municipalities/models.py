2632e1e3ca64691b374a93d7ac5fae3837d892b7
Mark Hildreth <mark.k.hildreth@gmail.com>
2013-06-03 19:54:11 -0400

    Fixed municipality columns to be closer to existing database.

--- frcog/municipalities/models.py
+++ frcog/municipalities/models.py
@@ -6,9 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 class Municipality(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='municiplaity_created_by')
+    created_by = models.ForeignKey(User, related_name='municiplaity_created_by', db_column='created_by')
     modified_on = models.DateTimeField(auto_now=True)
-    modified_by = models.ForeignKey(User, related_name='municiplaity_modified_by')
+    modified_by = models.ForeignKey(User, related_name='municiplaity_modified_by', db_column='modified_by')
 
     class Meta:
         db_table = 'municipalities'
